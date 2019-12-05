f7b76b2b647864e8b015ada678c57fc6929dd95b
Daniel <fcdfl1218@163.com>
2019-12-05 21:15:41 +0800

    添加employee路由

--- app.ts
+++ app.ts
@@ -5,8 +5,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import employeeRouter from './routes/employee';
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/employee', employeeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import  express from  'express';
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: any, res: any, next: any) {
   res.render('index', { title: 'TypeScript' });
 });
 
-module.exports = router;
+export default router;
